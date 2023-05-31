2ea59c3096b35532d76ca227b3a6c5cad7b26c3a
hollowdll <pixerchat@gmail.com>
2023-05-31 16:50:38 +0300

    first commit

---
+++ hello-world/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    private static void PrintHelloWorldUpperWithVariable()
+    {
+        string output = "hello world!";
+        Console.WriteLine(output.ToUpper());
+    }
+
+    private static void ParameterMethod(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+        PrintHelloWorldUpperWithVariable();
+        ParameterMethod("hello world!");
+    }
+}
